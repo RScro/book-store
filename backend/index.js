fcb6592438e021ee32b01b7ff9e2dfb54be552ba
RScro <rscro@protonmail.com>
2023-10-23 14:46:43 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
-app.get();
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN");
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
