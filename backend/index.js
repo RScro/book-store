4413bc3100c81446382a01f1046beed6479996ea
RScro <rscro@protonmail.com>
2023-10-24 12:37:47 +0200

    added post

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,43 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+// Middleware for parsing body request
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN");
 });
+
+// Route for Save a new Book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields, title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All Books from database
+app.get("/books");
 
 mongoose
   .connect(mongoDBURL)
